c350591c9e27c2a1d3a798d34257626a165ace4e
Silver Fan <silver.fan@team-c.com>
2022-06-07 15:54:00 +0800

    dockerfile & docker-compose

--- app.js
+++ app.js
@@ -50,5 +50,24 @@ app.post("/register", async (req, res) => {
 }) 
 
 app.post("/login", async (req, res) => {
-    
+    try {
+      const { email, password } = req.body;
+      if (!(email && password)) {
+        res.status(400).send("All input is required.");
+      }
+      const user = await User.findOne({email});
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+            { user_id: user._id, email}, 
+            process.env.TOKEN_KEY,
+            { expiresIn: "2h" });
+        user.token = token;
+
+        res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credential");
+    }
+    catch (err) {
+      console.log(err);
+    }
 }) ;
